0ae0da35d1d87bf7b7c5674ef4f96d7670b217f9
pol <pol.robledillo.7e7@itb.cat>
2024-09-18 19:28:33 +0200

    refactoring

---
+++ review_one/review_one/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string AvgMessage = "El promig de les notes es: ";
+        const string PassedMessage = "Aprovat";
+        const string FailedMessage = "Suspès";
+        string[] InputGradeMessages = { "Introdueix la primera qualificació", 
+                                        "Introdueix la segona qualificació", 
+                                        "Introdueix la tercera qualificació" };
+        int nota;
+        float avg = 0;
+
+        for (int i = 0; i < InputGradeMessages.Length; i++)
+        {
+            Console.WriteLine(InputGradeMessages[i]);
+            nota = Convert.ToInt32(Console.ReadLine());
+            avg += nota;
+        }
+
+        avg /= InputGradeMessages.Length;
+
+        Console.WriteLine(AvgMessage + avg);
+
+        Console.WriteLine(avg > 6 ? PassedMessage : FailedMessage);
+    }
+}
